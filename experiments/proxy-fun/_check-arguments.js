397ce40be8cc4eea8d99bd11bd5446c17a11649b
Atul Varma <varmaa@gmail.com>
2016-02-27 20:59:13 -0500

    add basic arg checking

--- experiments/proxy-fun/_check-arguments.js
+++ experiments/proxy-fun/_check-arguments.js
@@ -1,8 +1,57 @@
 define('check-arguments', function() {
-  function buildValidator(classitem) {
-    // TODO: Parse classitems.params[].type, validate it, make validators.
-    return {
+  var typeValidators = {
+    'Number': function(value) {
+      return typeof(value) === 'number';
+    },
+    'String': function(value) {
+      return typeof(value) === 'string';
+    },
+    'p5.Color': function(value) {
+      return value instanceof p5.Color;
+    },
+    'p5.Image': function(value) {
+      return value instanceof p5.Image;
+    }
+  };
 
+  function buildTypeValidator(type) {
+    var validators = type.split('|').map(function(type) {
+      if (typeof(typeValidators[type]) !== 'function')
+        throw new Error('Unknown type: ' + type);
+      return typeValidators[type];
+    });
+
+    return function(value) {
+      return validators.some(function(typeValidator) {
+        return typeValidator(value);
+      });
+    };
+  }
+
+  function buildParamValidator(classitem, param, argIndex) {
+    var isValidType = buildTypeValidator(param.type);
+
+    return function(value) {
+      if (!isValidType(value)) {
+        if (!param.optional) {
+          console.log("Invalid value for arg #" + (argIndex + 1) + " (" +
+                      param.name + ") of " + classitem.name + "()");
+        }
+      }
+    };
+  }
+
+  function buildValidator(classitem) {
+    var name = classitem.name;
+    var paramValidators = (classitem.params || [])
+      .map(buildParamValidator.bind(null, classitem));
+
+    return {
+      validate: function(target, thisArg, argumentsList) {
+        paramValidators.forEach(function(paramValidator, i) {
+          paramValidator(argumentsList[i], i);
+        });
+      }
     };
   };
 
@@ -10,7 +59,7 @@ define('check-arguments', function() {
     if (!classitem.validator)
       classitem.validator = buildValidator(classitem);
 
-    // TODO: Check arguments, provide feedback if needed.
+    classitem.validator.validate(target, thisArg, argumentsList);
   }
 
   checkArguments._buildValidator = buildValidator;
