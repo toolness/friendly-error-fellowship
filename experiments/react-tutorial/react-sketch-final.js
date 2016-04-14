09b78c0b160cc0b8e18525ab50c3fecadbbf598a
Atul Varma <varmaa@gmail.com>
2016-04-14 10:54:19 -0400

    Add react-tutorial experiment.

---
+++ experiments/react-tutorial/react-sketch-final.js
@@ -0,0 +1,47 @@
+var Sketch = React.createClass({
+  getInitialState: function() {
+    return {
+      circles: [],
+      fill: 'white'
+    };
+  },
+  handleMouseDown: function(event) {
+    this.setState({fill: 'black'});
+  },
+  handleMouseUp: function(event) {
+    this.setState({fill: 'white'});
+  },
+  handleMouseMove: function(event) {
+    var oldCircles = this.state.circles;
+    this.setState({
+      circles: oldCircles.concat({
+        key: oldCircles.length,
+        cx: event.clientX,
+        cy: event.clientY,
+        r: 50,
+        stroke: 'black',
+        fill: this.state.fill
+      })
+    });
+  },
+  render: function() {
+    var circles = this.state.circles.map(function(circle) {
+      return React.createElement('circle', circle);
+    });
+
+    return React.createElement('svg', {
+      onMouseMove: this.handleMouseMove,
+      onMouseUp: this.handleMouseUp,
+      onMouseDown: this.handleMouseDown,
+      style: {
+        width: '640px',
+        height: '480px'
+      }
+    }, circles);
+  }
+});
+
+ReactDOM.render(
+  React.createElement(Sketch),
+  document.getElementById('sketch')
+);
