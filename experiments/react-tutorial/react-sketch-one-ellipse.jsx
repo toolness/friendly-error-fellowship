c324342c479d9eedade3364e331eb69b2e3e1349
Atul Varma <varmaa@gmail.com>
2016-04-14 11:56:40 -0400

    add JSX version of one ellipse example

---
+++ experiments/react-tutorial/react-sketch-one-ellipse.jsx
@@ -0,0 +1,14 @@
+var Sketch = React.createClass({
+  render: function() {
+    return (
+      <svg width={640} height={480}>
+        <circle cx={50} cy={50} r={40} stroke="black" fill="white"/>
+      </svg>
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(Sketch),
+  document.getElementById('sketch')
+);
